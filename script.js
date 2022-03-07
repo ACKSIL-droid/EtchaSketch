dd07a47ea17d5d67780c160ee210940db1ebc21c
ACKSIL-droid <acbskinner@gmail.com>
2022-03-07 16:33:34 +0200

    Add div population function
    Adds a function that populates the screen with divs.

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-const wheel1 = document.querySelector('#wheel1');
-const wheel2 = document.querySelector('#wheel2');
+const wheels = document.querySelectorAll('.wheels');
 
-const wheel1Bounds = wheel1.getBoundingClientRect();
-const wheel2Bounds = wheel2.getBoundingClientRect();
-
-const wheel1Center = {
-    x: wheel1Bounds.left + wheel1Bounds.width/2,
-    y: wheel1Bounds.top + wheel1Bounds.height/2
-};
-
-const wheel2Center = {
-    x: wheel2Bounds.left + wheel2Bounds.width/2,
-    y: wheel2Bounds.top + wheel2Bounds.height/2
-};
-
-document.addEventListener('mousemove', e => {
-    let angle1 = Math.atan2(e.pageX - wheel1Center.x, - (e.pageY - wheel1Center.y)) * (180 / Math.PI);
-    let angle2 = Math.atan2(e.pageX - wheel2Center.x, - (e.pageY - wheel2Center.y)) * (180 / Math.PI);
-    wheel1.style.transform = `rotate(${angle1}deg)`;
-    wheel2.style.transform = `rotate(${angle2}deg)`;
+wheels.forEach( (wheel) => {
+    let wheelBounds = wheel.getBoundingClientRect();
+    let wheelCenter = {
+        x: wheelBounds.left + wheelBounds.width/2,
+        y: wheelBounds.top + wheelBounds.height/2
+    };
+    document.addEventListener('mousemove', e => {
+        let angle = Math.atan2(e.pageX - wheelCenter.x, - (e.pageY - wheelCenter.y)) * (180 / Math.PI);
+        wheel.style.transform = `rotate(${angle}deg)`;
+        })
 });
+
+
+const screen = document.getElementById('screen');
+const sizes = document.querySelectorAll('a');
+let gridSize = 16;
+
+function fillScreen (gridSize) {
+    for (let i = 0; i < gridSize**2; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        screen.appendChild(square);
+    }
+    
+}
+
+sizes.forEach( (size) => {
+    size.addEventListener('click', () => {
+        gridSize = size.id;
+        fillScreen(gridSize);
+    })
+});
+
+window.onload = () => {
+    fillScreen(gridSize);
+}
